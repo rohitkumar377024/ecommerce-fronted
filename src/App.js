8c02e8b7ad9c1600244e463e20c40ada48e3c3f6
HemantArya786 <hemantarya7860@gmail.com>
2022-11-28 18:30:30 +0530

    my ecommerce furniture

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import './App.css';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './pages/home';
-import About from './pages/About';
-import Contact from './pages/contact';
+import Navbar from "./components/Navbar";
+import Home from "./pages/home";
+import About from "./pages/About";
+import Contact from "./pages/contact";
 // import Category from './pages/category';
-import Loginpage from './pages/loginpage';
-import Register from './Register';
-import Cart from './pages/cart';
-import Productlist from './pages/productlist';
-import Products from './pages/products';
-
-
+import Loginpage from "./pages/loginpage";
+import Register from "./Register";
+import Cart from "./pages/cart";
+import Productlist from "./pages/productlist";
+import Products from "./pages/products";
 
 function App() {
-
-  let isLoggedIn = localStorage.getItem("isLoggedIn")
+  let isLoggedIn = localStorage.getItem("isLoggedIn");
   return (
-    <div className='h-100'>
+    <div className="h-100">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={ isLoggedIn ? <Home />: <Loginpage/>} />
-          <Route path="/about" element={ isLoggedIn ? <About />: <Loginpage/>} />
-          <Route path="/contact" element={  isLoggedIn ?   <Contact />  : <Loginpage/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           {/* <Route path="/category" element={ isLoggedIn ?   <Category /> : <Loginpage/>} />  */}
-          <Route path='/Loginpage' element={<Loginpage/>}/>
-          <Route path='/Register' element={<Register/>}/>
-          <Route path='/Cart' element={<Cart/>}/>
-          <Route path='/Productlist' element={<Productlist/>}/>
-          <Route path='/Products' element ={<Products/>}/>
+          <Route path="/Loginpage" element={<Loginpage />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Productlist" element={<Productlist />} />
+          <Route path="/Products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
